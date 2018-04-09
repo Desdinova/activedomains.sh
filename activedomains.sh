6171ac4cff96caa7f7fc612be5aceda1338574e5
Desdinova <florian@infra.net>
2018-04-09 15:09:59 +0200

    wrote script

--- activedomains.sh
+++ activedomains.sh
@@ -1,1 +1,8 @@
- 
+#!/bin/bash
+
+SERVERS=$(grep -hEr "ServerName|ServerAlias" /etc/apache2/sites-enabled/ | sed "s/ServerName//" | sed "s/ServerAlias//" | tr -d "[:blank:]")
+
+for DOMAIN in $SERVERS
+do
+  dig +noall +answer $DOMAIN
+done
