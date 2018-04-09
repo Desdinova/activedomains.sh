6723fd13825c70c753cd02a0fe14342eb20116fb
Desdinova <florian@infra.net>
2018-04-09 15:17:46 +0200

    added comments

--- activedomains.sh
+++ activedomains.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# grep for ServerName, ServerAlias, and clean output
 SERVERS=$(grep -hEr "ServerName|ServerAlias" /etc/apache2/sites-enabled/ | sed "s/ServerName//" | sed "s/ServerAlias//" | tr -d "[:blank:]")
 
+# check DNS A-Record for each domain
 for DOMAIN in $SERVERS
 do
   dig +noall +answer $DOMAIN
